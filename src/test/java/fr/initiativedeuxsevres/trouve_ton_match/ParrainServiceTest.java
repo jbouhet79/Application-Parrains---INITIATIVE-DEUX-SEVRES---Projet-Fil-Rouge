f55eb3e4c31edd1569a65c9126381e9b4e0c821e
Jerome BOUHET <jerome.bouhet@maif.fr>
2024-11-06 12:04:47 +0100

    fichier test pour la fonction completerCompteParrainÃ

--- src/test/java/fr/initiativedeuxsevres/trouve_ton_match/ParrainServiceTest.java
+++ src/test/java/fr/initiativedeuxsevres/trouve_ton_match/ParrainServiceTest.java
@@ -1,4 +1,56 @@
 package fr.initiativedeuxsevres.trouve_ton_match;
 
+import fr.initiativedeuxsevres.trouve_ton_match.entity.Parrain;
+import fr.initiativedeuxsevres.trouve_ton_match.repository.ParrainRepository;
+import fr.initiativedeuxsevres.trouve_ton_match.service.ParrainService;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 public class ParrainServiceTest {
+
+    @Mock
+    private ParrainRepository parrainRepository;
+
+    @InjectMocks
+    private ParrainService parrainService;
+
+    @Test
+    public void completerCompteParrain() {
+
+        Long idUtilisateur = 1L;
+
+        String presentationParcours = "Nouvelle Présentation";
+        String branchesReseau = "Nouveaux Branches";
+        String domainesExpertise = "Nouveaux Domaines";
+        String secteurGeographique = "Nouveau Secteur";
+        String disponibilites = "Nouvelles Disponibilités";
+
+        Parrain mockParrain = new Parrain(
+                "pres",
+                "TP",
+                "comptabilité",
+                "NIORT",
+                "5j");
+
+        when(parrainRepository.findById(idUtilisateur)).thenReturn(Optional.of(mockParrain));
+
+        Parrain parrain = parrainService.completerCompteParrain(
+                presentationParcours,
+                branchesReseau,
+                domainesExpertise,
+                secteurGeographique,
+                disponibilites);
+
+        assertEquals(presentationParcours, parrain.getPresentationParcours());
+        assertEquals(branchesReseau, parrain.getBranchesReseau());
+        assertEquals(domainesExpertise, parrain.getDomainesExpertise());
+        assertEquals(secteurGeographique, parrain.getSecteurGeographique());
+        assertEquals(disponibilites, parrain.getDisponibilites());
+    }
 }
