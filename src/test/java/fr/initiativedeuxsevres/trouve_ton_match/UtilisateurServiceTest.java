e004ed7806198930b4541e0f888fabdb2ced57a7
Jerome BOUHET <jerome.bouhet@maif.fr>
2024-11-06 17:01:41 +0100

    test: début du test pour la fonction sauvegarderTypeAccompagnement

--- src/test/java/fr/initiativedeuxsevres/trouve_ton_match/UtilisateurServiceTest.java
+++ src/test/java/fr/initiativedeuxsevres/trouve_ton_match/UtilisateurServiceTest.java
@@ -1,4 +1,23 @@
 package fr.initiativedeuxsevres.trouve_ton_match;
 
+import fr.initiativedeuxsevres.trouve_ton_match.service.UtilisateurService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class UtilisateurServiceTest {
+
+    @Mock
+    private UtilisateurRepository utilisateurRepository;
+
+    @InjectMocks
+    private UtilisateurService utilisateurService;
+
+    @Test
+    public void sauvegarderTypeAccompagnement() {
+
+    }
 }
